706ea313fa817d57fa34077b721c80e64a7c72c8
Filip Włodarczyk <116726173+fwlodarczyk@users.noreply.github.com>
2024-08-06 11:55:41 +0200

    Update 01-setup-ubuntu-22.04.sh

--- 01-setup-ubuntu-22.04.sh
+++ 01-setup-ubuntu-22.04.sh
@@ -7,6 +7,8 @@ sudo apt autopurge -y
 sudo apt install git curl htop iperf3 cloud-guest-util -y
 curl -s https://gist.githubusercontent.com/ikr4-m/67023682f949c2c22b0e51d0acb68b05/raw/506a888299e3e6e726a240e44d07d5d4bc900e87/install.sh | sudo bash
 curl -fsSL https://get.docker.com | sudo bash
-sudo usermod -aG docker $USER
 curl -s https://install.husarnet.com/install.sh | sudo bash
+su $SUDO_USER
+sudo usermod -aG docker $USER
+exit
 sudo poweroff
