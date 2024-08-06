4daf9cd4f748dac010b46928ba7b2abb5676a5e7
Filip Włodarczyk <116726173+fwlodarczyk@users.noreply.github.com>
2024-08-06 08:31:22 +0200

    Update ros2-humble-install.sh

--- ros2-humble-install.sh
+++ ros2-humble-install.sh
@@ -7,7 +7,8 @@ sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 sudo apt update && sudo apt full-upgrade -y && sudo snap refresh
 sudo apt install ros-humble-desktop -y
-# fix this to not execute as sudo
-echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
 sudo apt install ros-humble-rmw-cyclonedds-cpp -y
+su $SUDO_USER
+echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
+exit
 exit
