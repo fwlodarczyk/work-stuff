1452d4bee1a66be790247e57f5f06a71483ad06e
Filip Włodarczyk <116726173+fwlodarczyk@users.noreply.github.com>
2024-08-06 12:39:31 +0200

    Update 01-setup-ubuntu-22.04.sh

--- 01-setup-ubuntu-22.04.sh
+++ 01-setup-ubuntu-22.04.sh
@@ -4,7 +4,7 @@ sudo apt full-upgrade -y
 sudo snap refresh
 sudo apt install linux-azure -y
 sudo apt autopurge -y
-sudo apt install git curl htop iperf3 cloud-guest-util -y
+sudo apt install git curl htop iperf3 cloud-guest-utils -y
 curl -s https://gist.githubusercontent.com/ikr4-m/67023682f949c2c22b0e51d0acb68b05/raw/506a888299e3e6e726a240e44d07d5d4bc900e87/install.sh | sudo bash
 curl -fsSL https://get.docker.com | sudo bash
 curl -s https://install.husarnet.com/install.sh | sudo bash
